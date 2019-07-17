81e0178083a99ffa587048144818aa9255b0f46d
Nakib <kingamet91@gmail.com>
2019-07-17 15:33:52 +0200

    Footer Style

--- src/components/layout/navigation/twit-footer.js
+++ src/components/layout/navigation/twit-footer.js
@@ -1,15 +1,28 @@
 import { LitElement, html, css } from 'lit-element';
+import 'fa-icons';
 
 class TwitFooter extends LitElement {
 
 	constructor(){
         super();
         this.tab = "";
+        this.color = "#eeeeee";
+        this.size = "1.5em";
     }
-    
+
+   /* firstUpdated() {
+
+        document.addEventListener('connection-changed', ({detail}) => {
+            this.color = "#00BFFF";
+        });
+
+    }
+*/
     static get properties(){
         return {
-            tab: String
+            tab: String,
+            color: String,
+            size: String
         };
     }
 
@@ -25,16 +38,39 @@ class TwitFooter extends LitElement {
                 bottom: 0;
                 width: 100%;
                 height:9%;
+                border-top:solid 1px #eeeeee;
+                background-color: #fff;
+                box-shadow: 0px -1px 3px -2px black;
+                justify-content: center;
             }
+            
             footer > ul {
                 display: flex;
-                justify-content: space-between;
-                align-items: center;
+                justify-content: center;
+                align-items: unsafe center;
                 flex-wrap: wrap;
             }
+            
+            #icon-home{
+                size:2em;
+            }
             footer > ul > li {
-                flex-grow: 1;
+                float: left;
+                padding : 0px 8%;
+                flex-grow:1;
                 display: inline-block;
+                
+            }
+            @media screen and (min-width: 900px){
+                footer {
+                    height:5%;
+                }
+                footer > ul > li {
+                
+                padding : 0px 10%;
+                
+                
+            }
             }
         `;
     }
@@ -43,9 +79,9 @@ class TwitFooter extends LitElement {
 		return html`
 			<footer>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/profil">Profil</a></li>
-                    <li><a href="/post">New Tweet</a></li>
+                    <li><a href="/"><fa-icon id="icon-home" class="fas fa-home" color=${this.color} size=${this.size}></fa-icon></a></li>
+                    <li><a href="/profil"><fa-icon id="icon-user" class="fas fa-user" color=${this.color} size=${this.size}></fa-icon></a></li>
+                    <li><a href="/post"><fa-icon id="icon-add" class="fas fa-plus-circle" color=${this.color} size=${this.size}></fa-icon></a></li>
                 </ul>
             </footer>
 		`;
