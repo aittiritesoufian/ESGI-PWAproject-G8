bdea3f20f4ff2956e69f7bf754c472e18b64d84a
Soufian AIT TIRITE <aittirite.soufian@gmail.com>
2019-07-23 13:01:52 +0200

    update

--- src/components/views/twit-home.js
+++ src/components/views/twit-home.js
@@ -73,15 +73,14 @@ class TwitHome extends LitElement {
     }
 
     firstUpdated() {
+        document.addEventListener('user-logged', (event) => {
+            this.user = event.detail.user;
+        });
         if (firebase.auth().currentUser){
             this.user = firebase.auth().currentUser;
         }
         this.unresolved = false;
         this.logged = localStorage.getItem('logged') == "true" ? true : false;
-        document.addEventListener('user-logged', (event) => {
-            this.user = event.detail.user;
-        });
-        
     }
 
     urlBase64ToUint8Array(base64String) {
